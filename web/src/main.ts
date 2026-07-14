028870f24f152bd5d184dab88b25ce82bc8867ec
haru <azusachino@proton.me>
2026-07-14 12:16:46 +0900

    chore: finalize apps web relocation

--- web/src/main.ts
+++
@@ -1,8 +0,0 @@
-import 'maplibre-gl/dist/maplibre-gl.css'
-import './index.css'
-import App from './App.svelte'
-import { mount } from 'svelte'
-
-mount(App, {
-  target: document.getElementById('root')!,
-})
